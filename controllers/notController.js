f0983ce51b047e44d672c9764031aca99c473b88
moriel <morielsh57@gmail.com>
2021-08-24 23:59:33 +0300

    add todo

--- controllers/notController.js
+++ controllers/notController.js
@@ -5,6 +5,7 @@ exports.getNotifications = async (req, res) => {
   try {
     const user = await UserModel.findOne({ _id: req.userData._id })
     if (user.role !== "supplier") return res.json({ message: "You have to be a supplier" });
+    //need by the token of the supplier
     let data = await NotificationModel.find({});
     return res.status(200).json(data);
   }
