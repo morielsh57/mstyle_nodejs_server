3c51ed1dd8d34ca2fa44f3b72011b26bf82a0bb1
moriel <morielsh57@gmail.com>
2021-08-28 22:35:10 +0300

    add get all the main category that in the DB router

--- routes/categories.js
+++ routes/categories.js
@@ -11,6 +11,8 @@ router.get('/byMain/:main', categoryController.categoryByMain);
 
 router.get('/count', categoryController.categoryAmount);
 
+router.get('/AllMainCat', categoryController.getAllMainCategory);
+
 router.post("/", authToken,authAdminToken, categoryController.createCategory);
 
 router.put("/:shortID", authToken,authAdminToken, categoryController.editCategory);
